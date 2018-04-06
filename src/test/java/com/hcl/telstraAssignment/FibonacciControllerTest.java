2c7919bc711185599d64160ceb9ada505883e6a3
NitinNagaraj <38080703+NitinNagaraj@users.noreply.github.com>
2018-04-06 17:54:40 +0530

    code formatting done

--- src/test/java/com/hcl/telstraAssignment/FibonacciControllerTest.java
+++ src/test/java/com/hcl/telstraAssignment/FibonacciControllerTest.java
@@ -1,5 +1,8 @@
 package com.hcl.telstraAssignment;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -11,6 +14,9 @@ import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -22,30 +28,42 @@ import com.hcl.telstraAssignment.service.FibonacciService;
 @SpringBootTest
 @AutoConfigureMockMvc
 public class FibonacciControllerTest {
-	
+
 	String num = "10";
-	
-	@Autowired 
+
+	@Autowired
 	private MockMvc mockMvc;
-	
+
 	@Mock
 	private FibonacciController fibonacciController;
-	
+
 	@InjectMocks
 	private FibonacciService fibonacciService;
-	
-	@Before
-    public void setUp() throws Exception {
-        mockMvc = MockMvcBuilders.standaloneSetup(fibonacciController)
-                .build();
-    }
 
-	
-	@Test
-	public void testTen() throws Exception {
-		mockMvc.perform(get("/api/Fibonacci").param("n", num))
-		.andExpect(status().isOk());
+	@Before
+	public void setUp() throws Exception {
+		mockMvc = MockMvcBuilders.standaloneSetup(fibonacciController).build();
 	}
-	
-	
+
+	@Test
+	public void testSuccessScenario() {
+		assertEquals(89, fibonacciService.getNthFibonacciNumber(11));
+	}
+
+	@Test
+	public void testFailureScenario() {
+		assertNotEquals(89, fibonacciService.getNthFibonacciNumber(10));
+	}
+
+	@Test
+	public void testScenario1() throws Exception {
+		mockMvc.perform(get("/api/Fibonacci").param("n", num)).andExpect(status().isOk());
+	}
+
+	@Test
+	public void testGetNthFibonacciNumber() throws Exception {
+		given(this.fibonacciController.getNthFibonacciNumber("11")).willReturn(ResponseEntity.status(HttpStatus.OK)
+				.cacheControl(CacheControl.noCache()).header("Pragma", "no-cache").body(89));
+	}
+
 }
