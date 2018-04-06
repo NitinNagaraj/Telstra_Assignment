92ade463c8dd8bda1b616f1697f4e3f93d42f2ce
NitinNagaraj <38080703+NitinNagaraj@users.noreply.github.com>
2018-04-06 17:45:18 +0530

    Update FibonacciController.java

--- src/main/java/com/hcl/telstraAssignment/controller/FibonacciController.java
+++ src/main/java/com/hcl/telstraAssignment/controller/FibonacciController.java
@@ -1,5 +1,8 @@
 package com.hcl.telstraAssignment.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
@@ -11,19 +14,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.hcl.telstraAssignment.service.FibonacciService;
 
-
 @RestController
 @RequestMapping("/api")
 public class FibonacciController {
-	
+
 	@Autowired
 	private FibonacciService fibonacciService;
-	
+
 	@RequestMapping(value = "/Fibonacci", method = RequestMethod.GET)
-	public ResponseEntity<Long> getNthFibonacciNumber(@RequestParam("n")  long nthFibNum) throws IllegalArgumentException {
-		return ResponseEntity.status(HttpStatus.OK)
-                .cacheControl(CacheControl.noCache()).header("Pragma", "no-cache")
-                .body(fibonacciService.getNthFibonacciNumber(nthFibNum));
+	public ResponseEntity<Object> getNthFibonacciNumber(@RequestParam("n") Object nthIndexOfFibNum) {
+
+		try {
+			// converting the input to long type before getting the nth index
+			Long limit = new Long(nthIndexOfFibNum.toString());
+			return ResponseEntity.status(HttpStatus.OK).cacheControl(CacheControl.noCache())
+					.header("Pragma", "no-cache").body(fibonacciService.getNthFibonacciNumber(limit));
+			// Throw an invalid exception if the input is not proper
+		} catch (NumberFormatException numberException) {
+			Map<String, String> response = new HashMap<String, String>();
+			response.put("error", "Invalid input entered");
+			return ResponseEntity.badRequest().body(response);
+		}
 
 	}
 }
