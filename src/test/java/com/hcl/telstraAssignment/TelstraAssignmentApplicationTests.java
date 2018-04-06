0c648691cf214eb484506ea3d969f66a78d58817
NitinNagaraj <38080703+NitinNagaraj@users.noreply.github.com>
2018-04-06 17:57:56 +0530

    code formatted

--- src/test/java/com/hcl/telstraAssignment/TelstraAssignmentApplicationTests.java
+++ src/test/java/com/hcl/telstraAssignment/TelstraAssignmentApplicationTests.java
@@ -9,17 +9,32 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.hcl.telstraAssignment.controller.FibonacciController;
+import com.hcl.telstraAssignment.controller.ReverseWordsController;
+import com.hcl.telstraAssignment.controller.TriangleTypeController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TelstraAssignmentApplicationTests {
-	
+
 	@Autowired
 	private FibonacciController fibonacciController;
-	
+
+	@Autowired
+	private ReverseWordsController reverseWordsController;
+
+	@Autowired
+	private TriangleTypeController triangleTypeController;
+
 	@Test
 	public void contextLoads() {
 		assertNotNull(fibonacciController);
+		assertNotNull(reverseWordsController);
+		assertNotNull(triangleTypeController);
+	}
+
+	@Test
+	public void test() {
+		TelstraAssignmentApplication.main(new String[] { "--spring.main.web-environment=false", });
 	}
 
 }
