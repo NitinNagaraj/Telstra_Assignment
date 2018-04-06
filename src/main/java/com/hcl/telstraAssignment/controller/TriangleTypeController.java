5377cf9de0a4af1a7ec97e38c9a45d56e689ab9b
NitinNagaraj <38080703+NitinNagaraj@users.noreply.github.com>
2018-04-06 17:47:48 +0530

    code formatted

--- src/main/java/com/hcl/telstraAssignment/controller/TriangleTypeController.java
+++ src/main/java/com/hcl/telstraAssignment/controller/TriangleTypeController.java
@@ -1,5 +1,8 @@
 package com.hcl.telstraAssignment.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
@@ -11,19 +14,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.hcl.telstraAssignment.service.TriangleTypeService;
 
-
 @RestController
 @RequestMapping("/api")
 public class TriangleTypeController {
-	
+
 	@Autowired
 	private TriangleTypeService triangleTypeService;
-	
+
 	@RequestMapping(value = "/TriangleType", method = RequestMethod.GET)
-	public ResponseEntity<String> fetchTriangleType(@RequestParam("a")  int sideLength1,@RequestParam("b")  int sideLength2,@RequestParam("c")  int sideLength3) {
-		return ResponseEntity.status(HttpStatus.OK)
-                .cacheControl(CacheControl.noCache()).header("Pragma", "no-cache")
-                .body(triangleTypeService.getTriangleType(sideLength1, sideLength2, sideLength3));
+	public ResponseEntity<Object> getTriangleType(@RequestParam("a") Object sideLength1,
+			@RequestParam("b") Object sideLength2, @RequestParam("c") Object sideLength3) {
+		try {
+			// converting the input to integer type before getting the triangle type
+			Integer side1 = new Integer(sideLength1.toString());
+			Integer side2 = new Integer(sideLength2.toString());
+			Integer side3 = new Integer(sideLength3.toString());
+			return ResponseEntity.status(HttpStatus.OK).cacheControl(CacheControl.noCache())
+					.header("Pragma", "no-cache").body(triangleTypeService.getTriangleType(side1, side2, side3));
+		} catch (NumberFormatException numberException) {
+			Map<String, String> response = new HashMap<String, String>();
+			response.put("error", "Invalid input entered");
+			return ResponseEntity.badRequest().body(response);
+		}
 	}
-	
+
 }
