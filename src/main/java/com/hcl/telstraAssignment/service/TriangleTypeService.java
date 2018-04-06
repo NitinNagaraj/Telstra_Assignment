4eba1f9a56f62858303c00811b28391ce296570c
NitinNagaraj <38080703+NitinNagaraj@users.noreply.github.com>
2018-04-06 17:52:40 +0530

    code formatting done

--- src/main/java/com/hcl/telstraAssignment/service/TriangleTypeService.java
+++ src/main/java/com/hcl/telstraAssignment/service/TriangleTypeService.java
@@ -4,24 +4,26 @@ import org.springframework.stereotype.Service;
 
 /**
  * To return Three types of triangle - Equilateral, Isoceles or Scalene
+ * 
  * @param sideLength1
  * @param sideLength2
  * @param sideLength3
  * @return type of triangle as a String value
  */
 
-
 @Service
 public class TriangleTypeService {
-	
-	public String getTriangleType(int sideLength1, int sideLength2, int sideLength3) {
 
+	public String getTriangleType(int sideLength1, int sideLength2, int sideLength3) {
+		// to check if the triangle is equilateral
 		if (sideLength1 == sideLength2 && sideLength2 == sideLength3) {
 			return "Equilateral";
 		}
+		// to check if the triangle is iscoceles
 		else if (sideLength1 == sideLength2 || sideLength2 == sideLength3 || sideLength1 == sideLength3) {
 			return "Isosceles";
 		}
+		// to check if the triangle is scalene
 		else {
 			return "Scalene";
 		}
