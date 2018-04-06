648335e27b61483d038569a3efc12caaa3314e1d
NitinNagaraj <38080703+NitinNagaraj@users.noreply.github.com>
2018-04-06 17:56:28 +0530

    code added and code formatting done

--- src/test/java/com/hcl/telstraAssignment/TriangleTypeControllerTest.java
+++ src/test/java/com/hcl/telstraAssignment/TriangleTypeControllerTest.java
@@ -1,5 +1,8 @@
-/*package com.hcl.telstraAssignment;
+package com.hcl.telstraAssignment;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -11,6 +14,9 @@ import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -22,27 +28,76 @@ import com.hcl.telstraAssignment.service.TriangleTypeService;
 @SpringBootTest
 @AutoConfigureMockMvc
 public class TriangleTypeControllerTest {
-	
+
 	@Autowired
 	private MockMvc mockMvc;
-	
+
 	@Mock
 	private TriangleTypeController triangleTypeController;
-	
+
 	@InjectMocks
 	private TriangleTypeService triangleTypeService;
-	
+
 	@Before
-    public void setUp() throws Exception {
-        mockMvc = MockMvcBuilders.standaloneSetup(triangleTypeController)
-                .build();
-    }
-	
+	public void setUp() throws Exception {
+		mockMvc = MockMvcBuilders.standaloneSetup(triangleTypeController).build();
+	}
+
 	@Test
-	public void testTriangleTypeForEquilateral() throws Exception {
-		mockMvc.perform(get("/api/TriangleType?a=1&b=1&c=1"))
-		.andExpect(status().isOk());
+	public void testSuccessScenario1() throws Exception {
+		mockMvc.perform(get("/api/TriangleType").param("a", "1").param("b", "1").param("c", "1"))
+				.andExpect(status().is(200));
+	}
+
+	@Test
+	public void testTriangleTypePositiveScenarioWhenTriangleIsIsosceles() throws Exception {
+		given(this.triangleTypeController.getTriangleType("2", "1", "1"))
+				.willReturn(ResponseEntity.status(HttpStatus.OK).cacheControl(CacheControl.noCache())
+						.header("Pragma", "no-cache").body("Isosceles"));
+	}
+
+	@Test
+	public void testTriangleTypePositiveScenarioWhenTriangleIsEquilateral() throws Exception {
+		given(this.triangleTypeController.getTriangleType("2", "2", "2"))
+				.willReturn(ResponseEntity.status(HttpStatus.OK).cacheControl(CacheControl.noCache())
+						.header("Pragma", "no-cache").body("Equilateral"));
+	}
+
+	@Test
+	public void testTriangleTypePositiveScenarioWhenTriangleIsScalene() throws Exception {
+		given(this.triangleTypeController.getTriangleType("2", "3", "4"))
+				.willReturn(ResponseEntity.status(HttpStatus.OK).cacheControl(CacheControl.noCache())
+						.header("Pragma", "no-cache").body("Scalene"));
+	}
+
+	@Test
+	public void testForTriangleTypeEquilateral() {
+		assertEquals("Equilateral", triangleTypeService.getTriangleType(2, 2, 2));
+	}
+
+	@Test
+	public void testForTriangleTypeEquilateralFailureScenario() {
+		assertNotEquals("Equilateral", triangleTypeService.getTriangleType(2, 1, 2));
+	}
+
+	@Test
+	public void testForTriangleTypeIsosceles() {
+		assertEquals("Isosceles", triangleTypeService.getTriangleType(1, 2, 2));
+	}
+
+	@Test
+	public void testForTriangleTypeIsoscelesFailureScenario() {
+		assertNotEquals("Isosceles", triangleTypeService.getTriangleType(1, 3, 2));
+	}
+
+	@Test
+	public void testForTriangleTypeScalene() {
+		assertEquals("Scalene", triangleTypeService.getTriangleType(2, 3, 4));
+	}
+
+	@Test
+	public void testForTriangleTypeScaleneFailureScenario() {
+		assertNotEquals("Scalene", triangleTypeService.getTriangleType(2, 2, 2));
 	}
 
 }
-*/
