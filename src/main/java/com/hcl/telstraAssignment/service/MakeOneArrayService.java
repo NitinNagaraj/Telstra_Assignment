cfbf84234b2c7f80d9eec1d7fccfae58f094e470
NitinNagaraj <38080703+NitinNagaraj@users.noreply.github.com>
2018-04-18 18:12:35 +0530

    Update MakeOneArrayService.java

--- src/main/java/com/hcl/telstraAssignment/service/MakeOneArrayService.java
+++ src/main/java/com/hcl/telstraAssignment/service/MakeOneArrayService.java
@@ -1,41 +1,37 @@
 package com.hcl.telstraAssignment.service;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.hcl.telstraAssignment.model.RestRequestModel;
-import com.hcl.telstraAssignment.utils.MakeOneArrayServiceUtil;
+import com.hcl.telstraAssignment.model.RestResponseModel;
 
 @Service
 public class MakeOneArrayService {
-
+	private static final Logger LOGGER = LoggerFactory.getLogger(MakeOneArrayService.class);
 	List<Integer> resultList = new ArrayList<>();
 
-	public MakeOneArrayServiceUtil makeOneArray(RestRequestModel request) {
+	public RestResponseModel makeOneArray(RestRequestModel request) {
 
-		MakeOneArrayServiceUtil makeOneArrayUtil = new MakeOneArrayServiceUtil();
-		ArrayList<Integer> mergedList = new ArrayList<>();
-		mergedList.addAll(request.getArray1());
-		mergedList.addAll(request.getArray2());
-		mergedList.addAll(request.getArray3());
-		mergedList.addAll(request.getArray4());
-		mergedList.addAll(request.getArray5());
-		mergedList.addAll(request.getArray6());
-		mergedList.addAll(request.getArray7());
-		mergedList.addAll(request.getArray8());
-		mergedList.addAll(request.getArray9());
-		mergedList.addAll(request.getArray10());
-		Collections.sort(mergedList);
+		LOGGER.info("makeOneArray: Inside the makeOneArray method");
+		SortedSet<Integer> array = new TreeSet<Integer>();
+		LOGGER.debug("The values of request object is: " + request.getValues());
+		List<List<Integer>> list = new ArrayList<List<Integer>>(request.getValues().values());
+		Iterator<List<Integer>> iterator = list.iterator();
 
-		ArrayList<Integer> finalArrayList = new ArrayList<>();
-		for (Integer number : mergedList) {
-			if (!finalArrayList.contains(number))
-				finalArrayList.add(number);
+		while (iterator.hasNext()) {
+			array.addAll(iterator.next());
 		}
-		makeOneArrayUtil.setResultList(finalArrayList);
-		return makeOneArrayUtil;
+		RestResponseModel resp = new RestResponseModel();
+		resp.setArray1(array.toArray());
+		LOGGER.debug("The value of the response object is: " + resp.toString());
+		return resp;
 	}
 }
