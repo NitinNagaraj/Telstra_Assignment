f227296f2123c71b7d726cce9a95ca8d3eff442d
NitinNagaraj <38080703+NitinNagaraj@users.noreply.github.com>
2018-04-18 18:10:33 +0530

    Update RestRequestModel.java

--- src/main/java/com/hcl/telstraAssignment/model/RestRequestModel.java
+++ src/main/java/com/hcl/telstraAssignment/model/RestRequestModel.java
@@ -1,121 +1,26 @@
 package com.hcl.telstraAssignment.model;
 
+import java.io.Serializable;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class RestRequestModel {
+public class RestRequestModel implements Serializable {
 
-	@JsonProperty("Array1")
-	List<Integer> array1;
+	private static final long serialVersionUID = -8794413180188211951L;
 
-	@JsonProperty("Array2")
-	List<Integer> array2;
+	private Map<String, List<Integer>> values = new HashMap<String, List<Integer>>();
 
-	@JsonProperty("Array3")
-	List<Integer> array3;
-
-	@JsonProperty("Array4")
-	List<Integer> array4;
-
-	@JsonProperty("Array5")
-	List<Integer> array5;
-
-	@JsonProperty("Array6")
-	List<Integer> array6;
-
-	@JsonProperty("Array7")
-	List<Integer> array7;
-
-	@JsonProperty("Array8")
-	List<Integer> array8;
-
-	@JsonProperty("Array9")
-	List<Integer> array9;
-
-	@JsonProperty("Array10")
-	List<Integer> array10;
-
-	public List<Integer> getArray1() {
-		return array1;
+	@JsonAnyGetter
+	public Map<String, List<Integer>> getValues() {
+		return values;
 	}
 
-	public void setArray1(List<Integer> array1) {
-		this.array1 = array1;
+	@JsonAnySetter
+	public void setValues(String name, List<Integer> value) {
+		this.values.put(name, value);
 	}
-
-	public List<Integer> getArray2() {
-		return array2;
-	}
-
-	public void setArray2(List<Integer> array2) {
-		this.array2 = array2;
-	}
-
-	public List<Integer> getArray3() {
-		return array3;
-	}
-
-	public void setArray3(List<Integer> array3) {
-		this.array3 = array3;
-	}
-
-	public List<Integer> getArray4() {
-		return array4;
-	}
-
-	public void setArray4(List<Integer> array4) {
-		this.array4 = array4;
-	}
-
-	public List<Integer> getArray5() {
-		return array5;
-	}
-
-	public void setArray5(List<Integer> array5) {
-		this.array5 = array5;
-	}
-
-	public List<Integer> getArray6() {
-		return array6;
-	}
-
-	public void setArray6(List<Integer> array6) {
-		this.array6 = array6;
-	}
-
-	public List<Integer> getArray7() {
-		return array7;
-	}
-
-	public void setArray7(List<Integer> array7) {
-		this.array7 = array7;
-	}
-
-	public List<Integer> getArray8() {
-		return array8;
-	}
-
-	public void setArray8(List<Integer> array8) {
-		this.array8 = array8;
-	}
-
-	public List<Integer> getArray9() {
-		return array9;
-	}
-
-	public void setArray9(List<Integer> array9) {
-		this.array9 = array9;
-	}
-
-	public List<Integer> getArray10() {
-		return array10;
-	}
-
-	public void setArray10(List<Integer> array10) {
-		this.array10 = array10;
-	}
-
 }
