656ae477efa9287e24106b3746c12c0aca7a2b06
NitinNagaraj <38080703+NitinNagaraj@users.noreply.github.com>
2018-04-18 18:08:15 +0530

    Update MakeOneArrayController.java

--- src/main/java/com/hcl/telstraAssignment/controller/MakeOneArrayController.java
+++ src/main/java/com/hcl/telstraAssignment/controller/MakeOneArrayController.java
@@ -1,9 +1,7 @@
 package com.hcl.telstraAssignment.controller;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
@@ -15,24 +13,31 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hcl.telstraAssignment.model.RestRequestModel;
+import com.hcl.telstraAssignment.model.RestResponseModel;
 import com.hcl.telstraAssignment.service.MakeOneArrayService;
-import com.hcl.telstraAssignment.utils.MakeOneArrayServiceUtil;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
+@Api(value = "MakeOneArray")
 public class MakeOneArrayController {
-
+	private Logger LOGGER = LoggerFactory.getLogger(MakeOneArrayController.class);
 	@Autowired
 	private MakeOneArrayService makeOneArrayService;
 
+	@ApiOperation(value = "Merges all arrays into one", notes = "Program to combine all arrays into one by removing duplicates and maintaining order")
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Successs") })
 	@RequestMapping(value = "/api/makeonearray", method = RequestMethod.POST)
 	@ResponseBody
 	public ResponseEntity<Object> makeOneArray(@RequestBody RestRequestModel restRequest) {
-
-		Map<String, List<Integer>> finalResult = new HashMap<>();
-		MakeOneArrayServiceUtil restResponse = makeOneArrayService.makeOneArray(restRequest);
-		finalResult.put("Array", restResponse.getResultList());
+		LOGGER.info("makeOneArray: Inside the makeOneArray method of MakeOneArrayController");
+		RestResponseModel restResponse = makeOneArrayService.makeOneArray(restRequest);
+		LOGGER.debug("The final response from the service is:" + restResponse.getArray1().toString());
 		return ResponseEntity.status(HttpStatus.OK).cacheControl(CacheControl.noCache()).header("Pragma", "no-cache")
-				.body(finalResult);
+				.header("expires", "-1").body(restResponse);
 
 	}
 
